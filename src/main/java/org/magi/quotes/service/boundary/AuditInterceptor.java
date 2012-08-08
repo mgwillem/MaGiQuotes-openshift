8ab8df6e7e1a1b7eaaf482eca4ff0f95d7911c0c
Marc Gabriel-Willem <mgw@mmx.lu>
2012-08-08 15:57:07 +0200

    Testing interceptors on the cloud

--- src/main/java/org/magi/quotes/service/boundary/AuditInterceptor.java
+++ src/main/java/org/magi/quotes/service/boundary/AuditInterceptor.java
@@ -1,5 +1,9 @@
 package org.magi.quotes.service.boundary;
 
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
+import javax.ejb.SessionContext;
+import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
@@ -8,9 +12,12 @@ import javax.interceptor.InvocationContext;
  */
 public class AuditInterceptor {
 
+    @Resource
+    private EJBContext ejbContext;
+
     @AroundInvoke
     public Object interceptService(InvocationContext ctx) throws Exception {
-        System.out.println(":::Intercepting:::");
+        System.out.println(":::Intercepting:::" + ejbContext.getCallerPrincipal().getName());
         return ctx.proceed();
     }
 
